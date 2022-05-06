5b6a96645ff12e2ea915e9ab24fe2750629af261
Leonardo Soares <leonardo.oliveira@helpper.com.br>
2022-05-06 01:24:57 -0300

    Iniciando projeto e repositório

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AccountComponent } from './pages/account/account.component';
+import { BrandComponent } from './pages/brand/brand.component';
+import { HomeComponent } from './pages/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  {path: '', component:HomeComponent},
+  {path: 'account', component: AccountComponent},
+  {path: 'brand', component: BrandComponent}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './pages/home/home.component';
+import { AccountComponent } from './pages/account/account.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { BrandComponent } from './pages/brand/brand.component';
+import { SlidesComponent } from './components/slides/slides.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AccountComponent,
+    HeaderComponent,
+    FooterComponent,
+    BrandComponent,
+    SlidesComponent
   ],
   imports: [
     BrowserModule,
