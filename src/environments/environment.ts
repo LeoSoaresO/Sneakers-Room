6f4ad8bd4956c3406eac0693955b7e4397b48586
LeoSoaresO <leonardo.rock22@hotmail.com>
2024-02-23 14:57:17 -0300

    wip

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,11 +2,8 @@
 // `ng build` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
-export const environment = {
-  production: false
-};
-
 export const API = {
+  production: false,
   URL: "http://localhost:3000"
 }
 
