9c311751d60d82af4fec70778d750dc75bc9d12e
LeoSoaresO <leonardo.rock22@hotmail.com>
2024-02-23 22:26:14 -0300

    login page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,10 +4,12 @@ import { AccountComponent } from './pages/account/account.component';
 import { BrandComponent } from './pages/brand/brand.component';
 import { HomeComponent } from './pages/home/home.component';
 import { WomenComponent } from './components/women/women.component';
+import { LoginComponent } from './components/login/login.component';
 
 const routes: Routes = [
 
   {path: '', component:HomeComponent},
+  {path: 'login', component:LoginComponent},
   {path: 'account', component: AccountComponent},
   {path: 'brand', component: BrandComponent},
   {path: 'women', component: WomenComponent},
